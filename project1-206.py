0add1a7bfd58a686f06e4ab376826bff5b9a6a1b
jchiquit <44038004+jchiquit@users.noreply.github.com>
2018-10-23 20:57:17 -0400

    Update project1-206.py
    getData done

--- project1-206.py
+++ project1-206.py
@@ -2,6 +2,7 @@ import os
 import filecmp
 from dateutil.relativedelta import *
 from datetime import date
+import csv
 
 
 def getData(file):
@@ -9,16 +10,33 @@ def getData(file):
 #Input: file name
 #Ouput: return a list of dictionary objects where
 #the keys are from the first row in the data. and the values are each of the other rows
-
-	pass
+	File = open(file, "r")
+	s_infoLst = []
+	headers = File.readline()
+	lines = File.readlines()
+	for line in lines:
+		s_info = {}
+		item = line.split(",")
+		first = item[0]
+		last = item[1]
+		email = item[2]
+		c_year = item[3]
+		birthday = item[4]
+		line = File.readline()
+		s_info["First"] = first
+		s_info["Last"] = last
+		s_info["Email"] = email
+		s_info["Class"] = c_year
+		s_info["DOB"] = birthday
+		s_infoLst.append(s_info)
+	File.close()
+	return s_infoLst
 
 def mySort(data,col):
 # Sort based on key/column
 #Input: list of dictionaries and col (key) to sort on
 #Output: Return the first item in the sorted list as a string of just: firstName lastName
-
 	pass
-
 
 def classSizes(data):
 # Create a histogram
@@ -26,24 +44,20 @@ def classSizes(data):
 # Output: Return a list of tuples sorted by the number of students in that class in
 # descending order
 # [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
-
 	pass
-
 
 def findMonth(a):
 # Find the most common birth month form this data
 # Input: list of dictionaries
 # Output: Return the month (1-12) that had the most births in the data
-
 	pass
 
 def mySortPrint(a,col,fileName):
-#Similar to mySort, but instead of returning single
-#Student, the sorted data is saved to a csv file.
+# Similar to mySort, but instead of returning single
+# Student, the sorted data is saved to a csv file.
 # as fist,last,email
-#Input: list of dictionaries, col (key) to sort by and output file name
-#Output: No return value, but the file is written
-
+# Input: list of dictionaries, col (key) to sort by and output file name
+# Output: No return value, but the file is written
 	pass
 
 def findAge(a):
@@ -52,7 +66,6 @@ def findAge(a):
 # Output: Return the average age of the students and round that age to the nearest
 # integer.  You will need to work with the DOB and the current date to find the current
 # age in years.
-
 	pass
 
 
